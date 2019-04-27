10bbdda24ecc7d8a16eebfddd2b187b0cc5f6c06
Alan Bontoft <alan.bontoft@hotmail.co.uk>
2019-04-27 21:14:47 +0100

    Manual commit

--- WpfDataBoundApp/MyWindow.xaml.cs
+++ WpfDataBoundApp/MyWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace WpfDataBoundApp
 {
     /// <summary>
     /// Interaction logic for MyWindow.xaml
